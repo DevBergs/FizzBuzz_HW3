51d768426b8e02ade9422d38f9b270c2722131da
DevBergs <karlsbergsaivis@gmail.com>
2020-02-17 00:27:06 +0200

    Corections

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,35 @@
 console.log("Star Java Script Fizz Buzz");
 
-//izvilktie globalie no DOM
 const minInput = document.getElementById("minValue");
 const maxInput = document.getElementById("maxValue");
 const divEl = document.querySelector("#elements");
 
+// Individual Buttons
 // Listener Generate divs
-const btnGenerate = document.getElementById("btn-generate");
-btnGenerate.addEventListener("click", creatDivs);
-//Listener clear divs
+
+const btnAll = document.getElementById("btn-generate");
+btnAll.addEventListener("click", () => creatDivs(true, true, true, true));
+const btnFizzBuzz = document.getElementById("btn-generatefizzbuzz");
+btnFizzBuzz.addEventListener("click", () =>
+  creatDivs(true, false, false, false)
+);
+const btnFizz = document.getElementById("btn-generatefizz");
+btnFizz.addEventListener("click", () => creatDivs(false, false, true, false));
+const btnBuzz = document.getElementById("btn-generatebuzz");
+btnBuzz.addEventListener(
+  "click",
+  () => creatDivs(false, true, false, false),
+  myFunction
+);
 const btnClearDivs = document.getElementById("btn-cleardivs");
 btnClearDivs.addEventListener("click", onRemove);
-//Listeners Generate custom divs
-const btnGenerateCustom = document.getElementById("btn-generatefizzbuzz");
-btnGenerateCustom.addEventListener("click", createfizzbuzz);
-//Listeners Generate
-const btnFizz = document.getElementById("btn-generatefizz");
-btnFizz.addEventListener("click", genfizz);
 
 //Funktion Creating fizz, buzz, fizzbuzz, num
-function creatDivs() {
-  while (divEl.firstChild) {
-    divEl.removeChild(divEl.firstChild);
-  }
-  for (let i = 0; i <= 100; i++) {
+
+function creatDivs(genFizzBuzz, genFizz, genBuzz) {
+  onRemove();
+  console.log("Div`s Created! WooHoo");
+  for (let i = 1; i <= 100; i++) {
     let newDiv = document.createElement("div");
     let newPar = document.createElement("p");
     newDiv.appendChild(newPar);
@@ -31,13 +37,13 @@ function creatDivs() {
     if (i < minInput.value || i > maxInput.value) {
       newDiv.style = "display: none";
     } else {
-      if (i % 3 === 0 && i % 5 === 0) {
+      if (i % 3 === 0 && i % 5 === 0 && genFizzBuzz == true) {
         newPar.appendChild(document.createTextNode(i + " = FIZZ BUZZ"));
         divEl.appendChild(newDiv).setAttribute("class", "fizbuz");
-      } else if (i % 3 === 0) {
+      } else if (i % 3 === 0 && genFizz == true) {
         newPar.appendChild(document.createTextNode(i + " = FIZZ"));
         divEl.appendChild(newDiv).setAttribute("class", "fiz");
-      } else if (i % 5 === 0) {
+      } else if (i % 5 === 0 && genBuzz == true) {
         newPar.appendChild(document.createTextNode(i + " = BUZZ"));
         divEl.appendChild(newDiv).setAttribute("class", "buz");
       } else {
@@ -46,57 +52,30 @@ function creatDivs() {
       }
     }
   }
-  console.log("Div`s Created! WooHoo");
-}
-
-//Creating only fizzbuzz
-function createfizzbuzz() {
-  while (divEl.firstChild) {
-    divEl.removeChild(divEl.firstChild);
-  }
-  for (let i = 1; i <= 100; i++) {
-    let newDiv = document.createElement("div");
-    let newPar = document.createElement("p");
-    newDiv.appendChild(newPar);
-    divEl.appendChild(newDiv).setAttribute("id", "div_" + i);
-    if (i < minInput.value || i > maxInput.value) {
-      newDiv.style = "display: none";
-    } else {
-      if (i % 3 === 0 && i % 5 === 0) {
-        newPar.appendChild(document.createTextNode(i + " = FIZZ BUZZ"));
-        divEl.appendChild(newDiv).setAttribute("class", "fizbuz");
-      }
-    }
-  }
-  console.log("FizzBuzz! WooHoo");
-}
-
-//Creating only fizz
-function genfizz() {
-  while (divEl.firstChild) {
-    divEl.removeChild(divEl.firstChild);
-  }
-  for (let i = 1; i <= 100; i++) {
-    let newDiv = document.createElement("div");
-    let newPar = document.createElement("p");
-    newDiv.appendChild(newPar);
-    divEl.appendChild(newDiv).setAttribute("id", "div_" + i);
-    if (i < minInput.value || i > maxInput.value) {
-      newDiv.style = "display: none";
-    } else {
-      if (i % 3 === 0) {
-        newPar.appendChild(document.createTextNode(i + " = FIZZ"));
-        divEl.appendChild(newDiv).setAttribute("class", "fiz");
-      }
-    }
-  }
-  console.log("FizzBuzz! WooHoo");
 }
 
 //Clear divs by class name
 function onRemove() {
+  console.log("Divs Cleared! :)");
   while (divEl.firstChild) {
     divEl.removeChild(divEl.firstChild);
   }
-  console.log("Divs Cleared! :)");
 }
+
+//Delite num class ?????????? HOW
+
+function myFunction() {
+  var element = document.getElementsByClassName("num");
+  element.classList.remove("num");
+}
+
+function classDelite() {
+  console.log("num Delited!");
+  this.removeAttribute("num");
+}
+
+function main() {
+  console.log("Starting Listeners");
+}
+
+main();
